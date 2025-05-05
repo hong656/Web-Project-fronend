d588260c9c460de162e83b157cd363fe282a73c2
Heng <hengkov653@gmail.com>
2025-05-05 13:23:08 +0700

    a

--- lib/axios.ts
+++ lib/axios.ts
@@ -3,7 +3,7 @@ import axios from "axios";
 import Cookies from "js-cookie";
 
 const api = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_API_URL,
+  baseURL:'/api',
   withCredentials: true,
   headers: {
     "Content-Type": "application/json",
--- next.config.ts
+++ next.config.ts
@@ -5,6 +5,15 @@ const nextConfig: NextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
+
+  async rewrites() {
+    return [
+      {
+        source: "/api/:path*",
+        destination: `${process.env.NEXT_PUBLIC_API_URL}/:path*`, // Proxy to Backend
+      },
+    ];
+  }
 };
 
 export default nextConfig;
